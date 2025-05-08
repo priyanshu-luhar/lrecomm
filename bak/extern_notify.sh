177f28a32070010c401f489e95b6410b00ae4937
akoli-pilot <163882224+akoli-pilot@users.noreply.github.com>
2025-05-08 14:22:05 -0700

    Delete bak directory

--- bak/extern_notify.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-LOGFILE="/var/log/asterisk/extern_notify_test.log"
-echo "[$(date)] extern_notify triggered with args: $@" >> $LOGFILE
-
-# Lightweight enqueue step, pure Python, no Conda needed
-/usr/bin/python3 /home/mkausch/dev/3620/proj/lrecomm/enqueue_voicemail.py "$@" >> $LOGFILE 2>&1
