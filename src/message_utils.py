6be3732776d1d65e3e5800a7af2d17b540a508c1
priyanshu-luhar <69760063+priyanshu-luhar@users.noreply.github.com>
2025-05-10 13:14:35 -0700

    downgraded messages

--- src/message_utils.py
+++ src/message_utils.py
@@ -1,102 +1,81 @@
 import curses
 import textwrap
-import time
-import threading
-
-from globals import *
 from database_utils import get_messages
 from database_utils import log_msg_send
 
 WIDTH = 70
 FILLCHAR = " "
 SCROLL_STEP = 1
-box = ""
-
-
-def threaded_input(stdscr, stop_event, scroll_state):
-    global box
-    box = ""
-    stdscr.nodelay(True)
-
-    while not stop_event.is_set():
-        key = stdscr.getch()
-        if key == -1:
-            time.sleep(0.05)
-            continue
-        elif key in [10, 13]:  # ENTER
-            stop_event.set()
-            break
-        elif key in [curses.KEY_BACKSPACE, 127]:
-            box = box[:-1]
-        elif key == curses.KEY_UP:
-            scroll_state["pos"] = max(0, scroll_state["pos"] - SCROLL_STEP)
-        elif key == curses.KEY_DOWN:
-            scroll_state["pos"] = min(scroll_state["max"], scroll_state["pos"] + SCROLL_STEP)
-        elif 32 <= key <= 126:
-            box += chr(key)
-
 
 def show_messages(stdscr, identity_hash, NAME):
-    global box
-    box = ""
-    stop_event = threading.Event()
-    scroll_state = {"pos": 0, "max": 0, "user_scrolled": False, "last_msg_count": 0}
-
     curses.curs_set(1)
-    stdscr.nodelay(True)
-    curses.noecho()
+    stdscr.clear()
+    curses.echo()
 
     height, width = stdscr.getmaxyx()
     input_win_height = 8
     content_height = height - input_win_height - 5
 
-    # Launch input thread with scroll control
-    input_thread = threading.Thread(target=threaded_input, args=(stdscr, stop_event, scroll_state))
-    input_thread.start()
+    msg = get_messages(identity_hash)
 
-    while not stop_event.is_set():
-        stdscr.erase()
+    # Build wrapped and aligned message lines
+    formatted_lines = []
+    for x in msg:
+        wrapped = textwrap.wrap(x[0], WIDTH)
+        if x[2] == 1:
+            aligned = [line.rjust(width) for line in wrapped]
+            aligned.append(x[1].rjust(width))
+        else:
+            aligned = [line.ljust(width) for line in wrapped]
+            aligned.append(x[1].ljust(width))
+        formatted_lines.extend(aligned)
+
+    total_lines = len(formatted_lines)
+    scroll_pos = max(0, total_lines - content_height)
+
+    while True:
+        stdscr.clear()
 
         # Header
         stdscr.addstr(0, 0, "-" * width)
         stdscr.addstr(1, (width - len(NAME)) // 2, NAME)
         stdscr.addstr(2, 0, "-" * width)
 
-        # Messages
-        msg = get_messages(identity_hash)
-        formatted_lines = []
-        for x in msg:
-            wrapped = textwrap.wrap(x[0], WIDTH)
-            if x[2] == 1:
-                aligned = [line.rjust(width) for line in wrapped]
-                aligned.append(x[1].rjust(width))
-            else:
-                aligned = [line.ljust(width) for line in wrapped]
-                aligned.append(x[1].ljust(width))
-            formatted_lines.extend(aligned)
-
-        total_lines = len(formatted_lines)
-        scroll_state["max"] = max(0, total_lines - content_height)
-
-        # Auto-scroll to bottom if new message arrived and user didn't scroll manually
-        if total_lines != scroll_state.get("last_msg_count", 0):
-            scroll_state["pos"] = scroll_state["max"]
-        scroll_state["last_msg_count"] = total_lines
-        scroll_pos = scroll_state["pos"]
-        
+        # Message view
         view_lines = formatted_lines[scroll_pos:scroll_pos + content_height]
         for idx, line in enumerate(view_lines):
-            stdscr.addstr(3 + idx, 0, line[:width])
+            stdscr.addstr(3 + idx, 0, line[:width])  # Clip long lines
+            stdscr.addstr(3 + idx, 0, "")
 
-        # Input
+        # Input line
         stdscr.addstr(height - input_win_height - 2, 0, "-" * width)
-        stdscr.addstr(height - input_win_height - 1, 0, "[Type message + ENTER to send | Use ↑↓ to scroll]")
+        stdscr.addstr(height - input_win_height - 1, 0, "[type \"quit\" and press ENTER to exit] OR [press ENTER with no input to refresh for messages]")
         stdscr.addstr(height - input_win_height, 0, "Type your message: ")
-        stdscr.addstr(height - input_win_height, len("Type your message: "), box[:width - 20])
+        stdscr.clrtoeol()
         stdscr.refresh()
 
-        time.sleep(0.1)
-
-    input_thread.join()
-    return box.strip()
-
+        # Get user input char by char to handle scroll keys
+        win = curses.newwin(1, width - len("Type your message: ") - 1, height - input_win_height, len("Type your message: "))
+        curses.curs_set(1)
+        box = ""
+        while True:
+            key = stdscr.getch()
+            if key == curses.KEY_UP:
+                scroll_pos = max(0, scroll_pos - SCROLL_STEP)
+                break
+            elif key == curses.KEY_DOWN:
+                scroll_pos = min(total_lines - content_height, scroll_pos + SCROLL_STEP)
+                break
+            elif key == curses.KEY_BACKSPACE or key == 127:
+                box = box[:-1]
+                win.clear()
+                win.addstr(0, 0, box)
+                win.refresh()
+            elif key == curses.KEY_ENTER or key in [10, 13]:
+                user_input = box.strip()
+                stdscr.refresh()
+                return user_input
+            elif 32 <= key <= 126:  # Printable characters
+                box += chr(key)
+                win.addstr(0, len(box) - 1, chr(key))
+                win.refresh()
